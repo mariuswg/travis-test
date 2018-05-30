455e854de62615e4474ce6202c221bc718d41108
Marius Gynnild <mwgynnild@gmail.com>
2018-05-30 09:10:19 +0200

    Log even more

--- hello.js
+++ hello.js
@@ -5,3 +5,4 @@ http.createServer(function (req, res) {
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 console.log('More feedback');
+console.log('Was actually going to make this as well');
