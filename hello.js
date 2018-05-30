c4bda0256cad1a4ac783e50ba5d6ba9b3f45e61e
Marius Gynnild <mariuswg@users.noreply.github.com>
2018-05-30 09:30:18 +0200

    Log to console (#4)
    Write more describing text here...
    
    Changelog:
    
    * Log more feedback (#2)
    
    * Log more feedback
    
    * Fix syntax errors causing build to fail
    This did actually make the builds pass
    
    * Give even more feedback
    
    * Log even more (#3)
    
    Adapt to remove conflicts

--- hello.js
+++ hello.js
@@ -4,3 +4,6 @@ http.createServer(function (req, res) {
   res.end('Hello Travis!\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
+console.log('More feedback');
+console.log('Was actually going to make this as well');
+console.log('Even more feedback');
